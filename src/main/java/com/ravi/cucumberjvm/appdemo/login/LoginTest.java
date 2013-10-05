ad5533bd8c23024bc78fe7b65917fa91af8fc7ae
Ravi <usaravik@gmail.com>
2013-10-05 23:48:24 +1000

    Update LoginTest.java

--- src/main/java/com/ravi/cucumberjvm/appdemo/login/LoginTest.java
+++ src/main/java/com/ravi/cucumberjvm/appdemo/login/LoginTest.java
@@ -13,18 +13,18 @@ import cucumber.api.java.en.Then;
 
 public class LoginTest {
 	private static Logger LOGGER = Logger.getLogger(LoginTest.class);
-	private WebAppMediator webAppOrchestrate = WebAppMediatorImpl.getInstance();
+	private WebAppMediator webAppMediator = WebAppMediatorImpl.getInstance();
 	
 	@Given("^I open \"([^\"]*)\" and type \"([^\"]*)\"$")
 	public void I_Open(final String browser, final String url){
 		LOGGER.debug("I am going to "+ url +" on  "+ browser);
-		webAppOrchestrate.openBrowser(browser);
-		webAppOrchestrate.navigate(ApplicationProperties.getProperty(url));
+		webAppMediator.openBrowser(browser);
+		webAppMediator.navigate(ApplicationProperties.getProperty(url));
 	}
 	
 	@Then("^I should be \"([^\"]*)\" to login$")
 	public void I_should_be(String expectedResult){
 		LOGGER.debug("Login - "+ expectedResult);
-		Assert.assertEquals(expectedResult, webAppOrchestrate.isElementPresent("contactinfo")? "successfull": "failed");
+		Assert.assertEquals(expectedResult, webAppMediator.isElementPresent("contactinfo")? "successfull": "failed");
 	}
 }
