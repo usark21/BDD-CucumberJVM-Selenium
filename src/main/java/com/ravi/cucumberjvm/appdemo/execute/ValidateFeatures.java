c87b30867b5f98b67e21099feb3405a7f0ef0ec3
Ravi <usaravik@gmail.com>
2013-10-05 23:39:21 +1000

    Create ValidateFeatures.java

---
+++ src/main/java/com/ravi/cucumberjvm/appdemo/execute/ValidateFeatures.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format={"html:output"},
+		glue = {"com.ravi.cucumberjvm.appdemo", })
+public class ValidateFeatures {
+	
+}
