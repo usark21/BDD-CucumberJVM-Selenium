6a8b3e1a6eb1128423e645f0de295c42b8fd0bdd
Ravi <usaravik@gmail.com>
2013-10-10 12:04:58 +1100

    Update AdminMenuTest.java

--- src/main/java/com/ravi/cucumberjvm/appdemo/menu/AdminMenuTest.java
+++ src/main/java/com/ravi/cucumberjvm/appdemo/menu/AdminMenuTest.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 
-import com.ravi.cucumberjvm.appdemo.webinterface.WebAppMediator;
-import com.ravi.cucumberjvm.appdemo.webinterface.WebAppMediatorImpl;
+import com.ravi.cucumberjvm.appdemo.webinterface.WebAppDriver;
+import com.ravi.cucumberjvm.appdemo.webinterface.WebAppDriverImpl;
 
 import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
@@ -15,14 +15,14 @@ import cucumber.api.java.en.Then;
 
 public class AdminMenuTest {
 	private static Logger LOGGER = Logger.getLogger(AdminMenuTest.class);
-	private WebAppMediator webAppMediator = WebAppMediatorImpl.getInstance();
+	private WebAppDriver webAppDriver = WebAppDriverImpl.getInstance();
 	
 	@Given("^User logged in \"([^\"]*)\"$")
 	public void User_logged_in(String browser){
-		webAppMediator.openBrowser(browser);
-		if(!webAppMediator.isUserLoggedIn()){
+		webAppDriver.openBrowser(browser);
+		if(!webAppDriver.isUserLoggedIn()){
 			LOGGER.debug("Do default login");
-			webAppMediator.performDefaultLogin();
+			webAppDriver.performDefaultLogin();
 		}
 	}
 	
@@ -31,13 +31,13 @@ public class AdminMenuTest {
 		List<String> menus= dataTable.asList(String.class);
 		for(String menu : menus){
 			LOGGER.debug("Menu Item " + menu);
-			Assert.assertTrue("Element not found in Menu " + menu, webAppMediator.isElementPresent(menu));
+			Assert.assertTrue("Element not found in Menu " + menu, webAppDriver.isElementPresent(menu));
 		}
 	}
 	
 	@And("^\"([^\"]*)\" should be present$")
 	public void Should_be_present(String webElement){
 		LOGGER.debug("Web Element " + webElement + "should be present");
-		Assert.assertTrue("Element " + webElement + "not found", webAppMediator.isElementPresent(webElement));
+		Assert.assertTrue("Element " + webElement + "not found", webAppDriver.isElementPresent(webElement));
 	}
 }
