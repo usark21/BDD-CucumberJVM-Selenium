5160c6d6bb89bb2bd70bb83281010f49f0581dfb
Ravi <usaravik@gmail.com>
2013-10-10 12:06:26 +1100

    Update LoginTest.java

--- src/main/java/com/ravi/cucumberjvm/appdemo/login/LoginTest.java
+++ src/main/java/com/ravi/cucumberjvm/appdemo/login/LoginTest.java
@@ -4,8 +4,8 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 
 import com.ravi.cucumberjvm.appdemo.util.ApplicationProperties;
-import com.ravi.cucumberjvm.appdemo.webinterface.WebAppMediator;
-import com.ravi.cucumberjvm.appdemo.webinterface.WebAppMediatorImpl;
+import com.ravi.cucumberjvm.appdemo.webinterface.WebAppDriver;
+import com.ravi.cucumberjvm.appdemo.webinterface.WebAppDriverImpl;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -13,18 +13,18 @@ import cucumber.api.java.en.Then;
 
 public class LoginTest {
 	private static Logger LOGGER = Logger.getLogger(LoginTest.class);
-	private WebAppMediator webAppMediator = WebAppMediatorImpl.getInstance();
+	private WebAppDriver webAppDriver = WebAppDriverImpl.getInstance();
 	
 	@Given("^I open \"([^\"]*)\" and type \"([^\"]*)\"$")
 	public void I_Open(final String browser, final String url){
 		LOGGER.debug("I am going to "+ url +" on  "+ browser);
-		webAppMediator.openBrowser(browser);
-		webAppMediator.navigate(ApplicationProperties.getProperty(url));
+		webAppDriver.openBrowser(browser);
+		webAppDriver.navigate(ApplicationProperties.getProperty(url));
 	}
 	
 	@Then("^I should be \"([^\"]*)\" to login$")
 	public void I_should_be(String expectedResult){
 		LOGGER.debug("Login - "+ expectedResult);
-		Assert.assertEquals(expectedResult, webAppMediator.isElementPresent("contactinfo")? "successfull": "failed");
+		Assert.assertEquals(expectedResult, webAppDriver.isElementPresent("contactinfo")? "successfull": "failed");
 	}
 }
